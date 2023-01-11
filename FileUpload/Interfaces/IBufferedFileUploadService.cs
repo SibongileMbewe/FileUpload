d04756d7c2e20f54c292a4c089f13dde4519268d
Sibongile Mbewe <Sibongile@barko.co.za>
2023-01-11 15:55:46 +0200

    Add project files.

---
+++ FileUpload/Interfaces/IBufferedFileUploadService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Threading.Tasks;
+
+namespace FileUpload.Interfaces
+{
+    public interface IBufferedFileUploadService
+    {
+        Task<bool> UploadFile(IFormFile file);
+    }
+}
---
+++ FileUpload/Interfaces/IStreamFileUploadService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Threading.Tasks;
+
+namespace FileUpload.Interfaces
+{
+    public interface IStreamFileUploadService
+    {
+        Task<bool> UploadFile(MultipartReader reader, MultipartSection section);
+    }
+}
